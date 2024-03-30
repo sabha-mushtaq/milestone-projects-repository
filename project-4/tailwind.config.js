b30206ee35e272941499e5523cd651ae37ab95b0
sabha mushtaq <138353007+sabha-mushtaq@users.noreply.github.com>
2024-03-30 13:39:42 +0530

    Add files via upload

---
+++ project-4/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+colors : 
+{
+  purplem :'rgb(20, 1, 42)'
+}
+    },
+  },
+  plugins: [],
+}
+
